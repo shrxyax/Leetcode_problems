63a626f680f09c3e9d4be13cf3490e5d9c6bbfcc
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-20 03:23:03 +0530

    with and without recursion

--- Reverse-Linked-List.cpp
+++ Reverse-Linked-List.cpp
@@ -21,3 +21,23 @@ public:
         return newNode;
     }
 };
+
+
+//WITHOUT RECURSION
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL)
+        {
+            ListNode* nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        head=prev;
+        return prev;
+    }
+};
