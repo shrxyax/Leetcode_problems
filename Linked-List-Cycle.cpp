2d2e4c9bed2a57583dc044ca6e6c539a7beb4d37
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-21 19:50:32 +0530

    [141] [Time Beats: 39.78%] [Memory Beats: 22.94%] - LeetPush

--- Linked-List-Cycle.cpp
+++ Linked-List-Cycle.cpp
@@ -6,42 +6,20 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-class Solution {
+class Solution 
+{
 public:
     bool hasCycle(ListNode *head) 
     {
-       ListNode* tmp=head;
-       vector<ListNode*> v;
-       while(tmp!=NULL)
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast!=NULL && fast->next!=NULL)
        {
-          for(int i=0;i<v.size();i++)
-          {
-            if(tmp==v[i]) return true;
-          }
-          v.push_back(tmp);
-          tmp=tmp->next;
-       } 
+           fast= fast->next->next;
+           slow= slow->next;
+           if(fast==slow) return true;
+       }
        return false;
     }
 };
 
-//TWO POINTER APPROACH
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        while (fast != nullptr && fast->next != nullptr) {
-            fast = fast->next->next;
-            slow = slow->next;
-
-            if (fast == slow) {
-                return true;
-            }
-        }
-
-        return false;        
-    }
-};
