ba14fcb34ce72dba538163dc27170f266a081387
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-28 22:51:45 +0530

    [136] [Time Beats: 100.00%] [Memory Beats: 83.50%] - LeetPush

---
+++ Single-Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int ans=0;
+        for(int i:nums)
+        {
+            ans=ans^i;
+        }
+        return ans;
+    }
+};
+
+//brute force
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+};
