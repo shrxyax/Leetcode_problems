d8da7b3857f06fc08beb50573eaa61a9c85d706f
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-30 05:58:28 +0530

    [485] [Time Beats: 100.00%] [Memory Beats: 64.81%] - LeetPush

---
+++ Max-Consecutive-Ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
+        int c=0,max1=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1) c++;
+            else
+            {
+                max1=max(max1,c);
+                c=0;
+            }
+        }
+        max1=max(max1,c);
+        return max1;
+    }
+};
