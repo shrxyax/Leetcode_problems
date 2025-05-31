0ff84220e36d5058f0f1cdc08cfebcb491f84ec5
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-31 22:35:51 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 17.56%] - LeetPush

---
+++ Maximum-Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int res=nums[0];       //result should be initialized with first element
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(sum<0) sum=0;    //first we check if statement
+            sum=sum+nums[i];    //and then do sum
+            res=max(res,sum);   //otherwise there will error when entire array
+                                //is of negative numbers
+        }  
+        return res;     
+    }
+};
