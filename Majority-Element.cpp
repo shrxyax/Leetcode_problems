8ec14cbb1d0fa951804c353824a36aae61ac0c23
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-31 19:14:35 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 58.38%] - LeetPush

---
+++ Majority-Element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int el=0,count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0) el=nums[i];
+            if(nums[i]==el) count++;
+            else count--;
+        }
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==el) c++;
+        }
+        if(c>=(nums.size()/2)) return el;
+        return -1;
+    }
+};
