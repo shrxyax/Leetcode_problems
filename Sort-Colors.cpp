2c49ee4c1cbfea464b8ed9b011d75205d2b40c18
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-05-31 18:32:43 +0530

    dutch national flag algorithm

--- Sort-Colors.cpp
+++ Sort-Colors.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    void sortColors(vector<int>& nums) 
+    {
+       int count0=0,count1=0,count2=0;
+       for(int i=0;i<nums.size();i++)
+       {
+        if(nums[i]==0) count0++;
+        else if(nums[i]==1) count1++;
+        else count2++;
+       }
+       int i=0;
+       while(count0>0)
+       {
+        nums[i]=0;
+        i++;count0--;
+       }
+       while(count1>0)
+       {
+        nums[i]=1;
+        i++;count1--;
+       }
+       while(count2>0)
+       {
+        nums[i]=2;
+        i++;count2--;
+       }
+    }
+};
+
+
+class Solution {
+public:
 
     void sortColors(vector<int>& nums) 
     {
