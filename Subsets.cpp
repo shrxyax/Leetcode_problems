4363941c50be2492c0b712ebc30cf997832e4c65
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-06-09 06:12:47 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 60.64%] - LeetPush

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    void func(vector<int> &nums,vector<vector<int>> &result,int i,int n,vector<int> &ans)
+    {
+        if(i==n)
+        {
+            result.push_back(ans);
+            return;
+        }
+        ans.push_back(nums[i]);
+        func(nums,result,i+1,n,ans);
+        ans.pop_back();
+        func(nums,result,i+1,n,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>> result;
+        vector<int>ans;
+        int n=nums.size();
+        func(nums,result,0,n,ans);
+        return result;
+    }
+};
+
+//power set;
+
+class Solution {
+public:
     vector<vector<int>> subsets(vector<int>& nums) 
     {
         vector<vector<int>> result;
