52767dc2b7798f2abca14b8d03b3a124d03ccb08
shrxyax <162367874+shrxyax@users.noreply.github.com>
2025-06-13 04:45:37 +0530

    [60] [Time Beats: 100.00%] [Memory Beats: 55.79%] - LeetPush

--- Permutation-Sequence.cpp
+++ Permutation-Sequence.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    string permute(int &count, int idx, int n, int k, string s) {
-        if (idx == n) {
-            count++;
-            if (count == k) return s;
-            return \\;
-        }
-
-        // Sort the string starting from current index to maintain lex order
-        sort(s.begin() + idx, s.end());
-
-        for (int i = idx; i < n; i++) {
-            
-            swap(s[i], s[idx]);
-
-            string result = permute(count, idx + 1, n, k, s);
-            if (!result.empty()) return result;
-
-            swap(s[i], s[idx]); // Backtrack
-        }
-
-        return \\;
-    }
-
     string getPermutation(int n, int k) {
-        int count = 0;
+        vector<int> num;
         string s = \\;
-        for (int i = 1; i <= n; i++) s += (i + '0');
-        return permute(count, 0, n, k, s);
+        int f=1;
+        for (int i = 1; i < n; i++)
+        {
+            f=f*i;
+            num.push_back(i);
+        }
+        num.push_back(n);
+
+        k--;
+        while(true)
+        {
+            int index=k/f;
+            s+=to_string(num[index]);
+            num.erase(num.begin()+index);
+            if(num.empty()) break;
+            k=k%f;
+            f=f/num.size();
+        }
+        return s;
     }
 };
+
+//s += (num[index] + '0');
+//This is shorthand for:
+
+//s = s + (char)(num[index] + '0');
